9890664000c14168393673d646620ff2df974f1a
ritikgupta65 <157898550+ritikgupta65@users.noreply.github.com>
2025-03-10 15:28:06 +0530

    Update app.py
    from langchain_community.vectorstores import FAISS
    code is updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from PyPDF2 import PdfReader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
